15027bb70a406c702cea0d9bd0032012a15ca5c7
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-04-28 16:34:59 +1000

    Update rbenv path settings

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,11 +1,2 @@
 #!/bin/sh
-if [ -d "$HOME/.rbenv" ]; then
-  export PATH="$PATH:$HOME/.rbenv/bin"
-fi
-
-if which rbenv >/dev/null 2>&1; then
-  rbenv() {
-    eval "$(command rbenv init -)"
-    rbenv "$@"
-  }
-fi
+eval "$(rbenv init -)"
