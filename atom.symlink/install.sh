4c15464ec4f5ada63005ee399764087df6245527
Dylan Pinn <dylan.pinn@gmail.com>
2016-04-30 20:52:20 +1000

    Atom: remove installing packages as managed by package-sync

--- atom.symlink/install.sh
+++ atom.symlink/install.sh
@@ -2,16 +2,4 @@
 test "$(which apm)" || return 0
 apm update --confirm false
 apm install \
-  atom-beautify \
-  atom-wrap-in-tag \
-  color-picker \
-  editorconfig \
-  file-icons \
-  go-plus \
-  language-diff \
-  language-docker \
-  linter \
-  linter-ruby \
-  native-ui \
-  sort-lines \
   package-sync
