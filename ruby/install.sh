ca372ddd6c341797d03d516283cf3bb8818c6997
Dylan Pinn <dylan.pinn@gmail.com>
2016-04-30 20:51:54 +1000

    rbenv check if plugins exist

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 mkdir -p "$(rbenv root)/plugins"
-git clone https://github.com/rkh/rbenv-update.git "$(rbenv root)/plugins/rbenv-update"
-git clone https://github.com/rbenv/rbenv-default-gems.git $(rbenv root)/plugins/rbenv-default-gems
-git clone https://github.com/ianheggie/rbenv-binstubs.git $(rbenv root)/plugins/rbenv-binstubs
+if [ ! -d "$(rbenv root)/plugins/rbenv-update" ]; then
+  git clone https://github.com/rkh/rbenv-update.git "$(rbenv root)/plugins/rbenv-update"
+fi
+if [ ! -d "$(rbenv root)/plugins/rbenv-default-gems" ]; then
+  git clone https://github.com/rbenv/rbenv-default-gems.git "$(rbenv root)"/plugins/rbenv-default-gems
+fi
+if [ ! -d "$(rbenv root)/plugins/rbenv-binstubs" ]; then
+  git clone https://github.com/ianheggie/rbenv-binstubs.git "$(rbenv root)"/plugins/rbenv-binstubs
+fi
