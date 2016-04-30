60a04d2e822653634e14584bcac102672046db0d
Dylan Pinn <dylan.pinn@gmail.com>
2016-04-30 15:23:18 +1000

    Add aws config for osx

--- aws/completion.zsh
+++ aws/completion.zsh
@@ -1,2 +1,6 @@
 #!/bin/zsh
-source /home/dylan/.linuxbrew/share/zsh/site-functions/_aws
+if [ "$(uname -s)" = "Darwin" ]; then
+  source /usr/local/share/zsh/site-functions/_aws
+else
+  source /home/dylan/.linuxbrew/share/zsh/site-functions/_aws
+fi
