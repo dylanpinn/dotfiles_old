fe6de8d59069680f5681a2ce2e15a09f3226411f
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-05-10 14:30:35 +1000

    AWS: install via pip

--- aws/completion.zsh
+++ aws/completion.zsh
@@ -2,5 +2,5 @@
 if [ "$(uname -s)" = "Darwin" ]; then
   source /usr/local/share/zsh/site-functions/_aws
 else
-  source /home/dylan/.linuxbrew/share/zsh/site-functions/_aws
+  source /home/dylan/.pyenv/versions/2.7.11/bin/aws_zsh_completer.sh
 fi
