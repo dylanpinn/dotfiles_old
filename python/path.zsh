914b06eba3301de520ef0638e999be1c3d605606
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-05-16 10:40:54 +1000

    python: fix pyenv for osx and linux

--- python/path.zsh
+++ python/path.zsh
@@ -1,5 +1,11 @@
 #!/bin/sh
-pyenv() {
-  eval "$(command pyenv init -)"
-  pyenv "$@"
-}
+if [ "$(uname -s)" = "Darwin" ]; then
+  pyenv() {
+    eval "$(command pyenv init -)"
+    pyenv "$@"
+  }
+else
+  export PATH="$HOME/.pyenv/bin:$PATH"
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
+fi
