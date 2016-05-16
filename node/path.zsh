6f271231446cb88381ff539b6aae13a4e3c40c62
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-05-16 10:40:29 +1000

    node: fix nvm for linux and osx

--- node/path.zsh
+++ node/path.zsh
@@ -1,5 +1,10 @@
-#!/bin/sh
-export PATH="$PATH:/usr/local/share/npm/bin:$HOME/.npm/bin"
+#!/bin/zsh
+if [ "$(uname -s)" = "Darwin" ]; then
+  export PATH="$PATH:/usr/local/share/npm/bin:$HOME/.npm/bin"
 
-export NVM_DIR="$HOME/.nvm"
-  . "$(brew --prefix nvm)/nvm.sh"
+  export NVM_DIR="$HOME/.nvm"
+    . "$(brew --prefix nvm)/nvm.sh"
+else
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+fi
