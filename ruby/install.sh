c4f5dfcd536af504a142c891f0141fa1472f86a4
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-07-13 10:00:27 +1000

    Ruby: Comment out rbenv install

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-mkdir -p "$(rbenv root)/plugins"
-if [ ! -d "$(rbenv root)/plugins/rbenv-update" ]; then
-  git clone https://github.com/rkh/rbenv-update.git "$(rbenv root)/plugins/rbenv-update"
-fi
-if [ ! -d "$(rbenv root)/plugins/rbenv-default-gems" ]; then
-  git clone https://github.com/rbenv/rbenv-default-gems.git "$(rbenv root)"/plugins/rbenv-default-gems
-fi
-if [ ! -d "$(rbenv root)/plugins/rbenv-binstubs" ]; then
-  git clone https://github.com/ianheggie/rbenv-binstubs.git "$(rbenv root)"/plugins/rbenv-binstubs
-fi
+# TODO: Move to machine repo
+# mkdir -p "$(rbenv root)/plugins"
+# if [ ! -d "$(rbenv root)/plugins/rbenv-update" ]; then
+#   git clone https://github.com/rkh/rbenv-update.git "$(rbenv root)/plugins/rbenv-update"
+# fi
+# if [ ! -d "$(rbenv root)/plugins/rbenv-default-gems" ]; then
+#   git clone https://github.com/rbenv/rbenv-default-gems.git "$(rbenv root)"/plugins/rbenv-default-gems
+# fi
+# if [ ! -d "$(rbenv root)/plugins/rbenv-binstubs" ]; then
+#   git clone https://github.com/ianheggie/rbenv-binstubs.git "$(rbenv root)"/plugins/rbenv-binstubs
+# fi
