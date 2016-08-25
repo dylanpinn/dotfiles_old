3f7c1911259123bad3c1ef84e993e042194fc132
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-08-25 12:13:59 +1000

    VSCode: Install packages from textfile

--- vscode/install.sh
+++ vscode/install.sh
@@ -9,45 +9,11 @@ if test "$(which code)"; then
   ln -sf "$ZSH/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
   ln -sf "$ZSH/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
 
-  modules="
-    Borke.Puppet
-    CraigMaslowski.erb
-    DotJoshJohnson.xml
-    EditorConfig.EditorConfig
-    HookyQR.JSDocTagComplete
-    HookyQR.beautify
-    HvyIndustries.crane
-    Kasik96.format-indent
-    PeterJausovec.vscode-docker
-    WakaTime.vscode-wakatime
-    codezombiech.gitignore
-    dbaeumer.jshint
-    dbaeumer.vscode-eslint
-    deerawan.vscode-dash
-    donjayamanne.githistory
-    donjayamanne.python
-    eg2.tslint
-    felixfbecker.php-debug
-    georgewfraser.vscode-javac
-    haaaad.ansible
-    hoovercj.ruby-linter
-    ikappas.phpcs
-    joelday.docthis
-    linyang95.phpmd
-    lukehoban.Go
-    minhthai.vscode-todo-parser
-    misogi.ruby-rubocop
-    ms-vscode.cpptools
-    ms-vscode.csharp
-    ms-vscode.jscs
-    msjsdiag.debugger-for-chrome
-    rebornix.Ruby
-    robertohuertasm.vscode-icons
-    seanmcbreen.Spell
-    vsmobile.vscode-react-native
-    zhutian.swig
-  "
-  for module in $modules; do
-    code --install-extension "$module" || true
-  done
+  # Generate this list with `code --list-extensions`
+  filename="$ZSH/vscode/extensions.txt"
+  while read -r line
+  do
+      module="$line"
+      code --install-extension "$module" || true
+  done < "$filename"
 fi
