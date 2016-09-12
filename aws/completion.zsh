c289a8642b67460b0ee970b13ed10864af4a5d13
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-09-12 21:42:24 +1000

    AWS Check dir exists before loading autocomplete

--- aws/completion.zsh
+++ aws/completion.zsh
@@ -2,5 +2,7 @@
 if [ "$(uname -s)" = "Darwin" ]; then
   source /usr/local/share/zsh/site-functions/_aws
 else
-  source /home/dylan/.pyenv/versions/2.7.11/bin/aws_zsh_completer.sh
+  if [ -d "/home/dylan/.pyenv/versions/2.7.11/bin/" ]; then
+    source /home/dylan/.pyenv/versions/2.7.11/bin/aws_zsh_completer.sh
+  fi
 fi
