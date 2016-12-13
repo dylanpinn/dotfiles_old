2129d937aad702b93c80444e8fb4bc45ad4cd7d3
Dylan Pinn <dylan@dylanpinn.com>
2016-12-13 15:41:45 +1100

    Merge upstream

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if which brew >/dev/null 2>&1; then
-  brew untap getantibody/homebrew-antibody || true
-  brew tap getantibody/homebrew-antibody
+  brew untap -q getantibody/homebrew-antibody || true
+  brew tap -q getantibody/homebrew-antibody
   brew install antibody
 else
   curl -sL https://git.io/vwMNi | sh -s
 fi
 
-antibody bundle < "$ZSH/antibody/bundles.txt" > ~/.bundles.txt
+antibody bundle < "$DOTFILES/antibody/bundles.txt" > ~/.bundles.txt
 antibody bundle sindresorhus/pure >> ~/.bundles.txt
-antibody bundle < "$ZSH/antibody/last_bundles.txt" >> ~/.bundles.txt
+antibody bundle < "$DOTFILES/antibody/last_bundles.txt" >> ~/.bundles.txt
